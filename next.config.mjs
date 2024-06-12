46a8d2c103790af3aeb4adc43b23124b537da6dc
Fyodor Yemelyanenko <fyodor_e@hotmail.com>
2024-06-13 09:04:57 +1000

    Static HTML with updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://fyodore82.github.io/nextjs-react-router' : undefined,
+  distDir: 'docs',
   async rewrites() {
     return [
       {
